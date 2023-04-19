f5622df518117c58efb82d1ee095a6d1b8decfd5
KIK0M0RI <kudashev.020@gmail.com>
2023-04-19 12:35:02 +0300

    Initial commit

---
+++ src/main/java/com/kikomori/PpSpringBootCrudApplication.java
@@ -0,0 +1,12 @@
+package com.kikomori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpSpringBootCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpSpringBootCrudApplication.class, args);
+    }
+}
